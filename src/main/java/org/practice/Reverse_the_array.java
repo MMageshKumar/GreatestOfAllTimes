ef41b17f5b1051e0bb6037f5e78dc60892d2d37b
MMageshKumar <mageshkumarmurugaraj@outlook.com>
2025-12-10 08:59:46 +0530

    Prime Number

--- src/main/java/org/practice/Reverse_the_array.java
+++ src/main/java/org/practice/Reverse_the_array.java
@@ -5,5 +5,20 @@ import java.util.*;
 
 public class Reverse_the_array {
     public static void main(String[] args) {
+
+        // Prime numbers
+        // Rule is it can be able to divided by 1 or the same number
+
+        int number=11;
+
+        for (int i=2; i<number-1; i++){
+            if (number%i == 0){
+                System.out.println("Not a Prime number");
+                break;
+            } else {
+                System.out.println("Prime number");
+                break;
+            }
+        }
     }
 }
