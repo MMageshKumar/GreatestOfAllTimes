c30c33b0ba8993f5415e585810835f13a4b04ce8
MMageshKumar <mageshkumarmurugaraj@outlook.com>
2025-12-20 23:34:15 +0530

    Missing number Java Program

--- src/main/java/org/practice/Reverse_the_array.java
+++ src/main/java/org/practice/Reverse_the_array.java
@@ -1,24 +1,47 @@
 package org.practice;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.*;
 
 public class Reverse_the_array {
-    public static void main(String[] args) {
+public static void main(String[] args){
 
-        // Prime numbers
-        // Rule is it can be able to divided by 1 or the same number
+    int a[] = {1,2,3,4,5,7,8,9};
 
-        int number=11;
+    //n=n*n+1/2
+    //5*6= 30/2 = 15
 
-        for (int i=2; i<number-1; i++){
-            if (number%i == 0){
-                System.out.println("Not a Prime number");
-                break;
-            } else {
-                System.out.println("Prime number");
-                break;
-            }
-        }
+    int total = 0;
+
+    for (int x:a){
+        total = total+x;
+    };
+
+    int total2 =(a[a.length-1]*(a[a.length-1]+1))/2;
+//    System.out.println(total2);
+
+    int actualTotal = total2 - total;
+//System.out.println(actualTotal);
+
+    int b[]=new int[a.length+1];
+
+    int temp=0;
+    for (int x:a){
+        b[temp] = x;
+        temp++;
+    };
+
+    b[b.length-1] = actualTotal;
+
+    for(int y:b){
+        System.out.println(y);
     }
+
+   Arrays.sort(b);
+
+ String c=Arrays.toString(b);
+System.out.println(c);
+
+}
 }
